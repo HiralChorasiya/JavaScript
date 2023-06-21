a4819a5b3cb877638fbe7734218af9c76353c06a
HiralChorasiya <hiral.chorasiya@saeculumsolutions.com>
2023-06-21 12:31:02 +0530

    JS commit

---
+++ PRACTICE.js
@@ -0,0 +1,60 @@
+                                                           
+
+//Arithmetic opeators
+// let a=5;
+// let b=10;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(b%a);
+// console.log(++a);
+// console.log(--a);
+
+//Assignment opeators
+// let a=1;
+// let c=5;
+// console.log(a=c)
+// console.log(a+=c); 
+// console.log(a-=c);
+// console.log(a*=c);
+// console.log(a/=c);
+// console.log(c%=a);
+
+
+//Comparison operator
+// let a=10;
+// console.log (a==20);
+// console.log (a===20);
+// console.log (a!=20);
+// console.log (a!==20);
+// console.log (a>20);
+// console.log (a>=20);
+// console.log (a<20);
+// console.log (a<=20);
+
+//Logical Operators
+// let a=20;
+// console.log(a==20 && a==33);
+// console.log(a==20||a==33);
+// console.log(!(a==20));
+
+//Bitwise operators
+
+    // let x;
+    // x=5&1;
+    // x=5|1;
+// x=5^1;
+// x=~1;
+// x=5<<1;
+// x=5>>1;
+// x=5>>>1;
+
+    
+
+//Ternary or Conditional operator
+let age = 15;
+let result =
+    (age >= 18) ? "You are eligible to vote." : "You are not eligible to vote yet";
+console.log(result);
+
